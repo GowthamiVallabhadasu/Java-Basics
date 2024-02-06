c79afca0cb00b53464f51e2c51fc942bde365ccc
GowthamiVallabhadasu <gowthamivallabhadasu@gmail.com>
2024-02-07 04:50:01 +0530

    Added fibonacci logic

---
+++ Fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Fibonacci {
+    public static void main(String[] args) {
+        int num1=0,num2=0,num3,count;
+        System.out.println("Enter the count of fibonacci series\n");
+        Scanner s=new Scanner(System.in);
+        count=s.nextInt();
+        System.out.println("Fibonacci series are\n");
+        System.out.println(num1+" "+num2);
+        for (int i=2,i<count,++1)
+        {
+            
+        }
+    }
+}

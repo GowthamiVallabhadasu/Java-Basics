967305aa1d6e6868e318c048296d46b6732c828f
GowthamiVallabhadasu <gowthamivallabhadasu@gmail.com>
2024-02-07 03:41:46 +0530

    scanner example

---
+++ ScannerExample.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class myclass{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // declare an input variable 
+        int num =sc.nextInt();  
+        System.out.println("Entered inout is "+num);
+    }
+}

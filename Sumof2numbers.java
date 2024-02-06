976c0a8351181f518ee08ea405bc144c82941441
GowthamiVallabhadasu <gowthamivallabhadasu@gmail.com>
2024-02-07 03:50:29 +0530

    Added sum of 2 numbers

---
+++ Sumof2numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class test {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first num");
+        int num1=sc.nextInt();
+        System.out.println("Enter second number");
+        int num2 = sc.nextInt();
+        System.out.println("Sum of "+num1+" and "+num2+" is "+(num1+num2));
+    }
+}
